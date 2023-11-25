5a61b6e2527f211e77079949c3c2d34d81c7a327
Tyuiu.KalimullinaAH@outlook.com <boulevarovaalina@icloud.com>
2023-11-25 20:21:19 +0500

    Task 0

--- Tyuiu.KalimullinaAH.Sprint5.Task0.V29.Lib/DataService.cs
+++ Tyuiu.KalimullinaAH.Sprint5.Task0.V29.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.KalimullinaAH.Sprint5.Task0.V29.Lib
         public string SaveToFileTextData(int x)
         {
             string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
-            double z = 6.1 * Math.Pow (x, 3) + 0.23 * Math.Pow (x, 2) + 1.04 * Math.Pow (x, 3);
+            double z = 6.1 * Math.Pow (x,3) + 0.23 * Math.Pow (x, 2) + 1.04 * Math.Pow (x, 3);
             z = Math.Round(z, 3);
             File.WriteAllText(path, Convert.ToString(z));
             return path;
